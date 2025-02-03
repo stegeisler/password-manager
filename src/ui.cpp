8ca992538ada7b24a817083e7f02bedb78cfddc4
Stephan Geisler <stephan.geisler@fau.de>
2025-02-03 19:13:08 +0100

    Add request for master password on welcome message.

--- src/ui.cpp
+++ src/ui.cpp
@@ -8,4 +8,5 @@ CLI-based interface for Password Manager
 // Display message when starting Password Manager application
 void UI::displayWelcomeMessage() const {
     std::cout << "Welcome to the Password Manager!\n";
+    std::cout << "Please enter your master password to continue.\n";
 }
