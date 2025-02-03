ef3bdcd2f597acf16615ec3e21894d12c0dc0648
Stephan Geisler <stephan.geisler@fau.de>
2025-02-03 19:29:07 +0100

    Implement display menu function.

--- src/ui.cpp
+++ src/ui.cpp
@@ -10,3 +10,12 @@ void UI::displayWelcomeMessage() const {
     std::cout << "Welcome to the Password Manager!\n";
     std::cout << "Please enter your master password to continue.\n";
 }
+
+// Display the main menu options
+void UI::displayMenu() const {
+    std::cout << "\nMenu:\n";
+    std::cout << "1. Add a new password\n";
+    std::cout << "2. View a saved password\n";
+    std::cout << "3. Exit\n";
+    std::cout << "Please select an option: ";
+}
